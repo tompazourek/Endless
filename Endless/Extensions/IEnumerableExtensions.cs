7fb21ad4f3255192b80ffff7eb09cf9cb7bdc299
Tomáš Pažourek <tom.pazourek@gmail.com>
2013-11-03 13:00:55 +0100

    Added Enumerable.Concat with lazy second parameter.

--- Endless/Extensions/IEnumerableExtensions.cs
+++ Endless/Extensions/IEnumerableExtensions.cs
@@ -118,6 +118,23 @@ namespace Endless
         }
 
         /// <summary>
+        /// Concatenates two sequences with lazy second sequence.
+        /// </summary>
+        /// <param name="first">The first sequence to concatenate.</param>
+        /// <param name="second">The sequence to concatenate to the first sequence.</param>
+        /// <returns>
+        /// An <see cref="T:System.Collections.Generic.IEnumerable`1"/> that contains the concatenated elements of the two input sequences.
+        /// </returns>
+        public static IEnumerable<TSource> Concat<TSource>(this IEnumerable<TSource> first, Func<IEnumerable<TSource>> second)
+        {
+            foreach (TSource source in first)
+                yield return source;
+
+            foreach (TSource source in second())
+                yield return source;
+        }
+        
+        /// <summary>
         /// Returns all elements of the sequence from the begining until given element.
         /// </summary>
         /// <typeparam name="T"></typeparam>
