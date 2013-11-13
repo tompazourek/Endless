4326d392322aa6102cb585c1a0e10cfb48c9146d
Tomáš Pažourek <tom.pazourek@gmail.com>
2013-11-13 20:29:45 +0100

    Support for BitInteger for Enumerate. (fix #3)

--- Endless/Enumerate.cs
+++ Endless/Enumerate.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,38 @@ namespace Endless
 {
     public static class Enumerate
     {
+        #region BigInteger
+
+        public static IEnumerable<BigInteger> From(BigInteger n0)
+        {
+            return n0.Iterate(x => ++x);
+        }
+
+        public static IEnumerable<BigInteger> FromThen(BigInteger n0, BigInteger n1)
+        {
+            BigInteger distance = n1 - n0;
+            if (distance == 0)
+                return n0.Yield();
+
+            unchecked
+            {
+                return n0.Iterate(x => x + distance);
+            }
+        }
+
+        public static IEnumerable<BigInteger> FromTo(BigInteger n0, BigInteger m)
+        {
+            return FromThenTo(n0, ++n0, m);
+        }
+
+        public static IEnumerable<BigInteger> FromThenTo(BigInteger n0, BigInteger n1, BigInteger m)
+        {
+            Func<BigInteger, bool> predicate = n0 > n1 ? (x => x >= m) : new Func<BigInteger, bool>(x => x <= m);
+            return FromThen(n0, n1).TakeWhile(predicate);
+        }
+
+        #endregion
+
         #region int
 
         public static IEnumerable<int> From(int n0)
