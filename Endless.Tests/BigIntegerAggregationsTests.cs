55bd556151d957b45eafb3fcfd0422d85d61b356
Tomáš Pažourek <tom.pazourek@gmail.com>
2013-11-26 14:10:58 +0100

    Implemented BigInteger aggregations (Sum, Min, Max). (fix #12)

---
+++ Endless.Tests/BigIntegerAggregationsTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Endless.Tests
+{
+    [TestFixture]
+    public class BigIntegerAggregationsTests
+    {
+        [Test]
+        public void Sum()
+        {
+            // arrange
+            var bigNumber = new BigInteger(Int64.MaxValue);
+            var bigNumbers = bigNumber.Repeat().Take(10);
+
+            // act
+            var sum = bigNumbers.Sum();
+
+            // assert
+            Assert.AreEqual(bigNumber * 10, sum);
+        }
+
+        [Test]
+        public void Sum_Nullable()
+        {
+            // arrange
+            var bigNumber = new BigInteger(Int64.MaxValue);
+            var bigNumbers = new BigInteger?[] { bigNumber, null, bigNumber, bigNumber, null, null, bigNumber };
+
+            // act
+            var sum = bigNumbers.Sum();
+
+            // assert
+            Assert.AreEqual(bigNumber * 4, sum);
+        }
+
+        [Test]
+        public void Min()
+        {
+            // arrange
+            var bigNumber = new BigInteger(Int64.MaxValue);
+            var bigNumbers = new[] { bigNumber + 1, bigNumber + 3, bigNumber + 5, bigNumber + 10 };
+
+            // act
+            BigInteger min = bigNumbers.Min();
+
+            // assert
+            Assert.AreEqual(bigNumber + 1, min);
+        }
+
+        [Test]
+        public void Min_Empty()
+        {
+            // arrange
+            var bigNumbers = Enumerable.Empty<BigInteger>();
+
+            // act & assert
+            Assert.Throws<InvalidOperationException>(() => bigNumbers.Min());
+        }
+
+        [Test]
+        public void Min_Nullable()
+        {
+            // arrange
+            var bigNumber = new BigInteger(Int64.MaxValue);
+            var bigNumbers = new BigInteger?[] { bigNumber + 1, null, bigNumber + 3, bigNumber + 5, bigNumber + 10 };
+
+            // act
+            BigInteger? min = bigNumbers.Min();
+
+            // assert
+            Assert.AreEqual(bigNumber + 1, min);
+        }
+
+        [Test]
+        public void Min_Nullable_Empty()
+        {
+            // arrange
+            var bigNumbers = Enumerable.Empty<BigInteger?>();
+
+            // act
+            BigInteger? min = bigNumbers.Min();
+
+            // assert
+            Assert.IsNull(min);
+        }
+
+        [Test]
+        public void Max()
+        {
+            // arrange
+            var bigNumber = new BigInteger(Int64.MaxValue);
+            var bigNumbers = new[] { bigNumber + 1, bigNumber + 3, bigNumber + 5, bigNumber + 10 };
+
+            // act
+            BigInteger max = bigNumbers.Max();
+
+            // assert
+            Assert.AreEqual(bigNumber + 10, max);
+        }
+
+        [Test]
+        public void Max_Empty()
+        {
+            // arrange
+            var bigNumbers = Enumerable.Empty<BigInteger>();
+
+            // act & assert
+            Assert.Throws<InvalidOperationException>(() => bigNumbers.Max());
+        }
+
+        [Test]
+        public void Max_Nullable()
+        {
+            // arrange
+            var bigNumber = new BigInteger(Int64.MaxValue);
+            var bigNumbers = new BigInteger?[] { bigNumber + 1, null, bigNumber + 3, bigNumber + 5, bigNumber + 10 };
+
+            // act
+            BigInteger? max = bigNumbers.Max();
+
+            // assert
+            Assert.AreEqual(bigNumber + 10, max);
+        }
+
+        [Test]
+        public void Max_Nullable_Empty()
+        {
+            // arrange
+            var bigNumbers = Enumerable.Empty<BigInteger?>();
+
+            // act
+            BigInteger? max = bigNumbers.Max();
+
+            // assert
+            Assert.IsNull(max);
+        }
+    }
+}
---
+++ Endless/Extensions/BigIntegerAggregations.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endless
+{
+    /// <summary>
+    /// Variants of Min, Max and Sum with BigInteger.
+    /// </summary>
+    public static class BigIntegerAggregations
+    {
+        /// <summary>
+        /// Computes the sum of a sequence of <see cref="BigInteger"/> values.
+        /// </summary>
+        /// <param name="source">A sequence of <see cref="BigInteger"/> values to calculate the sum of.</param>
+        /// <returns>The sum of the values in the sequence.</returns>
+        public static BigInteger Sum(this IEnumerable<BigInteger> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            BigInteger result = source.Aggregate(new BigInteger(0), (x, y) => x + y);
+            return result;
+        }
+
+        /// <summary>
+        /// Computes the sum of a sequence of nullable <see cref="BigInteger"/> values.
+        /// </summary>
+        /// <param name="source">A sequence of nullable <see cref="BigInteger"/> values to calculate the sum of.</param>
+        /// <returns> he sum of the values in the sequence.</returns>
+        public static BigInteger? Sum(this IEnumerable<BigInteger?> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            BigInteger result = source
+                .Where(x => x.HasValue)
+                .Aggregate<BigInteger?, BigInteger>(0, (x, y) => x + y.GetValueOrDefault());
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the minimum value in a sequence of <see cref="BigInteger"/> values.
+        /// </summary>
+        /// <param name="source">A sequence of <see cref="BigInteger"/> values to determine the minimum value of.</param>
+        /// <returns>The minimum value in the sequence.</returns>
+        public static BigInteger Min(this IEnumerable<BigInteger> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            BigInteger minimum = 0;
+            bool isFirstItemProcessed = false;
+            foreach (BigInteger currentItem in source)
+            {
+                if (isFirstItemProcessed)
+                {
+                    if (currentItem < minimum)
+                        minimum = currentItem;
+                }
+                else
+                {
+                    minimum = currentItem;
+                    isFirstItemProcessed = true;
+                }
+            }
+
+            if (!isFirstItemProcessed)
+                throw new InvalidOperationException("Sequence does not contain any elements");
+
+            return minimum;
+        }
+
+        /// <summary>
+        /// Returns the minimum value in a sequence of nullable <see cref="BigInteger"/> values.
+        /// </summary>
+        /// <param name="source">A sequence of nullable <see cref="BigInteger"/> values to determine the minimum value of.</param>
+        /// <returns>The minimum value in the sequence.</returns>
+        public static BigInteger? Min(this IEnumerable<BigInteger?> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            BigInteger? minimum = null;
+            foreach (var currentItem in source)
+            {
+                if (!currentItem.HasValue)
+                    continue;
+
+                if (!minimum.HasValue || currentItem.Value < minimum.Value)
+                    minimum = currentItem;
+            }
+            return minimum;
+        }
+
+        /// <summary>
+        /// Returns the maximum value in a sequence of <see cref="BigInteger"/> values.
+        /// </summary>
+        /// <param name="source">A sequence of <see cref="BigInteger"/> values to determine the maximum value of.</param>
+        /// <returns>The maximum value in the sequence.</returns>
+        public static BigInteger Max(this IEnumerable<BigInteger> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            BigInteger maximum = 0;
+            bool isFirstItemProcessed = false;
+            foreach (BigInteger currentItem in source)
+            {
+                if (isFirstItemProcessed)
+                {
+                    if (currentItem > maximum)
+                        maximum = currentItem;
+                }
+                else
+                {
+                    maximum = currentItem;
+                    isFirstItemProcessed = true;
+                }
+            }
+
+            if (!isFirstItemProcessed)
+                throw new InvalidOperationException("Sequence does not contain any elements");
+
+            return maximum;
+        }
+
+        /// <summary>
+        /// Returns the maximum value in a sequence of nullable <see cref="BigInteger"/> values.
+        /// </summary>
+        /// <param name="source">A sequence of nullable <see cref="BigInteger"/> values to determine the maximum value of.</param>
+        /// <returns>The maximum value in the sequence.</returns>
+        public static BigInteger? Max(this IEnumerable<BigInteger?> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            BigInteger? maximum = null;
+            foreach (var currentItem in source)
+            {
+                if (!currentItem.HasValue)
+                    continue;
+
+                if (!maximum.HasValue || currentItem.Value > maximum.Value)
+                    maximum = currentItem;
+            }
+            return maximum;
+        }
+    }
+}
