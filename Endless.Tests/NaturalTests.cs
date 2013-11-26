7a335f3c97a8bebda5703549bf16513f1f465b3e
Tomáš Pažourek <tom.pazourek@gmail.com>
2013-11-26 12:19:35 +0100

    Completed tests for Natural and BigNatural numbers. (#1)

---
+++ Endless.Tests/NaturalTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Endless.Tests
+{
+    [TestFixture]
+    public class NaturalTests
+    {
+        [Test]
+        public void NaturalNumbers_10()
+        {
+            // act
+            var numbers = Natural.Numbers.Take(10);
+
+            // assert
+            CollectionAssert.AreEqual(new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, numbers);
+        }
+
+        [Test]
+        public void NaturalNumbersWithZero_10()
+        {
+            // act
+            var numbers = Natural.NumbersWithZero.Take(10);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, numbers);
+        }
+
+        [Test]
+        public void BigNaturalNumbers_10()
+        {
+            // act
+            var numbers = BigNatural.Numbers.Take(10);
+
+            // assert
+            CollectionAssert.AreEqual(new BigInteger[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, numbers);
+        }
+
+        [Test]
+        public void BigNaturalNumbersWithZero_10()
+        {
+            // act
+            var numbers = BigNatural.NumbersWithZero.Take(10);
+
+            // assert
+            CollectionAssert.AreEqual(new BigInteger[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, numbers);
+        }
+    }
+}
