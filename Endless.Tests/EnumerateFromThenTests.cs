0a69194ba04d4b677ca1cb016231a655788c28bb
Tomáš Pažourek <tom.pazourek@gmail.com>
2013-11-26 13:32:14 +0100

    Enumerate.FromThen and Enumerate.FromThenTo tests. (#1)

---
+++ Endless.Tests/EnumerateFromThenTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Endless.Tests
+{
+    [TestFixture]
+    public class EnumerateFromThenTests
+    {
+        [TestFixtureSetUp]
+        public void RunBeforeAnyTest()
+        {
+            GlobalSettings.DefaultFloatingPointTolerance = 0.01;
+        }
+
+        [Test]
+        public void EnumerateBigInteger_FromThen()
+        {
+            // act
+            IEnumerable<BigInteger> numbers = Enumerate.FromThen(new BigInteger(1), 3).Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new BigInteger[] { 1, 3, 5, 7, 9 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateByte_FromThen()
+        {
+            // act
+            IEnumerable<byte> numbers = Enumerate.FromThen((byte) 1, (byte) 3).Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new byte[] { 1, 3, 5, 7, 9 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateByte_FromThen_Overflow()
+        {
+            // act
+            byte overflow = Enumerate.FromThen((byte) 1, (byte) 2).ElementAt(255);
+
+            // assert
+            Assert.AreEqual(0, overflow);
+        }
+
+        [Test]
+        public void EnumerateChar_FromThen()
+        {
+            // act
+            IEnumerable<char> numbers = Enumerate.FromThen('a', 'c').Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 'a', 'c', 'e', 'g', 'i' }, numbers);
+        }
+
+        [Test]
+        public void EnumerateDecimal_FromThen()
+        {
+            // act
+            IEnumerable<decimal> numbers = Enumerate.FromThen(1, 1.1M).Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 1, 1.1M, 1.2M, 1.3M, 1.4M }, numbers);
+        }
+
+        [Test]
+        public void EnumerateDouble_FromThen()
+        {
+            // act
+            IEnumerable<double> numbers = Enumerate.FromThen(1d, 0.9d).Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 1, 0.9d, 0.8d, 0.7d, 0.6d }, numbers);
+        }
+
+        [Test]
+        public void EnumerateFloat_FromThen()
+        {
+            // act
+            IEnumerable<float> numbers = Enumerate.FromThen(1, 10f).Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new float[] { 1, 10, 19, 28, 37 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateInt_FromThen()
+        {
+            // act
+            IEnumerable<int> numbers = Enumerate.FromThen(1, -1).Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 1, -1, -3, -5, -7 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateLong_FromThen()
+        {
+            // act
+            IEnumerable<long> numbers = Enumerate.FromThen(1L, 1L).Take(5);
+
+            // assert
+            CollectionAssert.AreEqual(new long[] { 1 }, numbers);
+        }
+    }
+}
---
+++ Endless.Tests/EnumerateFromThenToTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Endless.Tests
+{
+    [TestFixture]
+    public class EnumerateFromThenToTests
+    {
+        [TestFixtureSetUp]
+        public void RunBeforeAnyTest()
+        {
+            GlobalSettings.DefaultFloatingPointTolerance = 0.01;
+        }
+
+        [Test]
+        public void EnumerateBigInteger_FromThenTo()
+        {
+            // act
+            IEnumerable<BigInteger> numbers = Enumerate.FromThenTo(new BigInteger(1), 3, 9);
+
+            // assert
+            CollectionAssert.AreEqual(new BigInteger[] { 1, 3, 5, 7, 9 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateByte_FromThenTo()
+        {
+            // act
+            IEnumerable<byte> numbers = Enumerate.FromThenTo((byte) 1, (byte) 3, (byte) 9);
+
+            // assert
+            CollectionAssert.AreEqual(new byte[] { 1, 3, 5, 7, 9 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateByte_FromThenTo_Empty()
+        {
+            // act
+            IEnumerable<byte> numbers = Enumerate.FromThenTo((byte) 253, (byte) 254, (byte) 3);
+
+            // assert
+            CollectionAssert.IsEmpty(numbers);
+        }
+
+        [Test]
+        public void EnumerateChar_FromThenTo()
+        {
+            // act
+            IEnumerable<char> numbers = Enumerate.FromThenTo('a', 'b', 'f');
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 'a', 'b', 'c', 'd', 'e', 'f' }, numbers);
+        }
+
+        [Test]
+        public void EnumerateDecimal_FromThenTo()
+        {
+            // act
+            IEnumerable<decimal> numbers = Enumerate.FromThenTo(1, 1.1M, 2M);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 1, 1.1M, 1.2M, 1.3M, 1.4M, 1.5M, 1.6M, 1.7M, 1.8M, 1.9M, 2M }, numbers);
+        }
+
+        [Test]
+        public void EnumerateDouble_FromThenTo()
+        {
+            // act
+            IEnumerable<double> numbers = Enumerate.FromThenTo(1, 0.5, -1);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 1, 0.5, 0, -0.5, -1 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateFloat_FromThenTo()
+        {
+            // act
+            IEnumerable<float> numbers = Enumerate.FromThenTo(1, 10f, 40);
+
+            // assert
+            CollectionAssert.AreEqual(new float[] { 1, 10, 19, 28, 37 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateInt_FromThenTo()
+        {
+            // act
+            IEnumerable<int> numbers = Enumerate.FromThenTo(1, -1, -10);
+
+            // assert
+            CollectionAssert.AreEqual(new[] { 1, -1, -3, -5, -7, -9 }, numbers);
+        }
+
+        [Test]
+        public void EnumerateLong_FromThenTo()
+        {
+            // act
+            IEnumerable<long> numbers = Enumerate.FromThenTo(1L, 1, 1);
+
+            // assert
+            CollectionAssert.AreEqual(new long[] { 1 }, numbers);
+        }
+    }
+}
