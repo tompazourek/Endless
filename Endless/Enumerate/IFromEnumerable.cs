26c33a00f1f9d2e428063e17a59c1d99e312cb03
Tomáš Pažourek <tom.pazourek@gmail.com>
2013-12-01 22:01:54 +0100

    Documentation.

--- Endless/Enumerate/IFromEnumerable.cs
+++ Endless/Enumerate/IFromEnumerable.cs
@@ -9,6 +9,10 @@ namespace Endless
 {
     public interface IFromEnumerable<T> : IFromThenEnumerable<T>
     {
+        /// <summary>
+        /// Endless collection of numbers starting with the n0 and n1, incremented by their distance.
+        /// </summary>
+        /// <returns>(n0, n1, n1 + distance, n1 + distance + distance, ...) where distance = n1 - n0</returns>
         IFromThenEnumerable<T> Then(T thenNumber);
     }
 }
--- Endless/Enumerate/IFromThenEnumerable.cs
+++ Endless/Enumerate/IFromThenEnumerable.cs
@@ -9,6 +9,10 @@ namespace Endless
 {
     public interface IFromThenEnumerable<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Bounds the collection of numbers from the right.
+        /// </summary>
+        /// <returns>(n0, ..., m)</returns>
         IEnumerable<T> To(T toNumber);
     }
 }
