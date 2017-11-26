06121d5aa110428c545e4527cb6961cb49724b0b
Tom Pažourek <tom.pazourek@gmail.com>
2017-11-26 16:33:39 +0100

    Added comparers to Sort method.

--- src/Endless/EnumerableExtensions/EnumerableExtensions.Sort.cs
+++ src/Endless/EnumerableExtensions/EnumerableExtensions.Sort.cs
@@ -16,12 +16,30 @@ namespace Endless
         }
 
         /// <summary>
+        /// Sorts the elements of the sequence in ascending order
+        /// </summary>
+        public static IEnumerable<T> Sort<T>(this IEnumerable<T> source, IComparer<T> comparer)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            return source.OrderBy(Function<T>.Identity, comparer);
+        }
+
+        /// <summary>
         /// Sorts the elements of the sequence in descending order
         /// </summary>
         public static IEnumerable<T> SortDescending<T>(this IEnumerable<T> source)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
             return source.OrderByDescending(Function<T>.Identity);
+        }
+
+        /// <summary>
+        /// Sorts the elements of the sequence in descending order
+        /// </summary>
+        public static IEnumerable<T> SortDescending<T>(this IEnumerable<T> source, IComparer<T> comparer)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            return source.OrderByDescending(Function<T>.Identity, comparer);
         }
     }
 }
